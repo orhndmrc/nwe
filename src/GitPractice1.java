129a265485f4664e7331138d58f7c9823295101c
Chunwewin <chunwe1993@gmail.com>
2023-09-27 17:16:21 -0400

    check again

--- src/GitPractice1.java
+++ src/GitPractice1.java
@@ -2,12 +2,7 @@
 public class GitPractice1 {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("mucahiteeee");
-=======
-		System.out.println("mucahitccc");
->>>>>>> master
-
 	}
 
 }
